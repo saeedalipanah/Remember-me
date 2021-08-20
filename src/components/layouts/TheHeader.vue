caace2dc0dd8c628b1cf649c03c336a1e3b8b515
saeed alipanah <saeedalipanah1374@gmail.com>
2021-08-20 08:03:47 -0700

    first commit

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ['title']
+}
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 7rem;
+  font-size: 110%;
+  /* background-color: #640032; */
+  background-color: #080055;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header h1 {
+  color: white;
+  margin: 0;
+}
+</style>
